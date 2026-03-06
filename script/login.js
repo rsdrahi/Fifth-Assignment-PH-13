2e04f2e9a09618651b087dfa4d2d6278b9465209
rsdrahi <rsdrahi13@gmail.com>
2026-03-07 02:25:12 +0600

    log in section dynamically with addEventListerner and successfully logged In.

--- script/login.js
+++ script/login.js
@@ -1,1 +1,19 @@
 // console.log('connected')
+
+document.getElementById("signin-btn").addEventListener('click', function () {
+  const userNameInput = document.getElementById("username-input");
+  const admin = userNameInput.value;
+  console.log(admin);
+
+  const passwordInput = document.getElementById("password-input");
+  const password = passwordInput.value;
+  console.log(password);
+
+  if (admin == "admin" && password == "admin123") {
+   alert("login successful") 
+  }
+  else {
+    alert("login failed");
+    return
+  }
+})
